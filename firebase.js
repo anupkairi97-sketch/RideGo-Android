f1801077eb8aaca791c41d5098c0119dfcef6d97
anupkairi97-sketch <anupkairi97@gmail.com>
2026-07-16 16:12:08 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBaEBZn9-uTCnASTpKBf5j04WHwWPq5tUA",
+  authDomain: "ridego-da1e6.firebaseapp.com",
+  projectId: "ridego-da1e6",
+  storageBucket: "ridego-da1e6.firebasestorage.app",
+  messagingSenderId: "427118307731",
+  appId: "1:427118307731:web:b9809c5e3af3297ceea066"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
