c2c730f6adec6718a353361d5d6856312383c980
anupkairi97-sketch <anupkairi97@gmail.com>
2026-07-16 20:15:22 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBaEBZn9-uTCnASTpKBf5j04WHwWPq5tUA",
@@ -12,8 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-
 export default app;
